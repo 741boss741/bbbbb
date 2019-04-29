f27ce9b9968543a079911142d1d12f1246e20a39
741boss741 <47608727+741boss741@users.noreply.github.com>
2019-04-29 03:34:06 +0200

    Add files via upload

---
+++ 1pX8Zdrw.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+client.on('message' , message => {
+  var prefix = "#";//البرفكس
+  if(message.author.bot) return;
+  if(message.content.startsWith(prefix + "ping")) {
+ message.channel.send('Pong...').then((msg) => {
+      msg.edit(`\`\`\`javascript\nTime taken: ${msg.createdTimestamp - message.createdTimestamp} ms.\nDiscord API: ${Math.round(client.ping)} ms.\`\`\``);//حقوق دايموند كودز
+ })
+  }  
+ });
+
+
+
+
+
+client.login('token');
